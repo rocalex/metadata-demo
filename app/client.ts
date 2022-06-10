95ab59ab738b1eaa2fcd649fccb25035f6e67710
rocalex <rocmaildev@gmail.com>
2022-06-10 17:26:08 +0300

    update anchor

--- app/client.ts
+++ app/client.ts
@@ -1,33 +1,52 @@
-import * as anchor from '@project-serum/anchor'
-import { Program } from '@project-serum/anchor'
-import { NodeWallet } from '@project-serum/common'
-import { ASSOCIATED_TOKEN_PROGRAM_ID, TOKEN_PROGRAM_ID } from "@solana/spl-token";
-import { MetadataProgram, DataV2, Metadata, MasterEdition } from "@metaplex-foundation/mpl-token-metadata";
-import { MetadataDemo } from '../target/types/metadata_demo'
+import * as dotenv from 'dotenv'
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
+import {
+  ASSOCIATED_TOKEN_PROGRAM_ID,
+  TOKEN_PROGRAM_ID,
+} from "@solana/spl-token";
+import {
+  MetadataProgram,
+  DataV2,
+  Metadata,
+  MasterEdition,
+} from "@metaplex-foundation/mpl-token-metadata";
+import { MetadataDemo } from "../target/types/metadata_demo";
 
-const options = anchor.Provider.defaultOptions()
-const connection = new anchor.web3.Connection("https://api.devnet.solana.com", options.commitment)
-const wallet = NodeWallet.local()
-const provider = new anchor.Provider(connection, wallet, options)
+dotenv.config()
 
-anchor.setProvider(provider)
+const options = anchor.AnchorProvider.defaultOptions();
+const connection = new anchor.web3.Connection(
+  "https://api.devnet.solana.com",
+  options.commitment
+);
+const wallet = anchor.Wallet.local();
+const provider = new anchor.AnchorProvider(connection, wallet, options);
+
+anchor.setProvider(provider);
 
 async function main() {
   const program = anchor.workspace.MetadataDemo as Program<MetadataDemo>;
 
-  console.log(program.programId.toBase58())
+  console.log(program.programId.toBase58());
   const mint = anchor.web3.Keypair.generate();
-  const payer = program.provider.wallet.publicKey;
+  const payer = provider.wallet.publicKey;
 
-  const [authority] = (await anchor.web3.PublicKey.findProgramAddress([
-    Buffer.from("auth"),
-  ], program.programId));
+  const [authority] = await anchor.web3.PublicKey.findProgramAddress(
+    [Buffer.from("auth")],
+    program.programId
+  );
 
-  const tokenAccount = (await anchor.web3.PublicKey.findProgramAddress([
-    authority.toBuffer(),
-    TOKEN_PROGRAM_ID.toBuffer(),
-    mint.publicKey.toBuffer()
-  ], ASSOCIATED_TOKEN_PROGRAM_ID))[0];
+  const tokenAccount = (
+    await anchor.web3.PublicKey.findProgramAddress(
+      [
+        authority.toBuffer(),
+        TOKEN_PROGRAM_ID.toBuffer(),
+        mint.publicKey.toBuffer(),
+      ],
+      ASSOCIATED_TOKEN_PROGRAM_ID
+    )
+  )[0];
 
   const data = new DataV2({
     name: "Collection",
@@ -36,23 +55,27 @@ async function main() {
     sellerFeeBasisPoints: 1000,
     creators: null,
     collection: null,
-    uses: null
+    uses: null,
   });
 
   const metadataAccount = await Metadata.getPDA(mint.publicKey);
   const editionAccount = await MasterEdition.getPDA(mint.publicKey);
 
-  // @ts-ignore
-  const tx = await program.methods.createMasterEdition(data, true, null).accounts({
-    authority,
-    mint: mint.publicKey,
-    tokenAccount,
-    metadataAccount,
-    editionAccount,
-    metadataProgram: MetadataProgram.PUBKEY,
-  }).signers([mint]).rpc();
+  const tx = await program.methods
+    // @ts-ignore
+    .createMasterEdition(data, true, null)
+    .accounts({
+      authority,
+      mint: mint.publicKey,
+      tokenAccount,
+      metadataAccount,
+      editionAccount,
+      metadataProgram: MetadataProgram.PUBKEY,
+    })
+    .signers([mint])
+    .rpc();
   console.log("Your transaction signature", tx);
 }
 
-console.log("Running client.")
-main().then(() => console.log("Success"))
+console.log("Running client.");
+main().then(() => console.log("Success"));
--- tests/metadata-demo.ts
+++ tests/metadata-demo.ts
@@ -1,35 +1,48 @@
-import * as anchor from '@project-serum/anchor';
-import { Program } from '@project-serum/anchor';
-import { ASSOCIATED_TOKEN_PROGRAM_ID, TOKEN_PROGRAM_ID } from "@solana/spl-token";
-import { MetadataProgram, DataV2, Metadata, MasterEdition } from "@metaplex-foundation/mpl-token-metadata";
-import { MetadataDemo } from '../target/types/metadata_demo';
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
+import {
+  ASSOCIATED_TOKEN_PROGRAM_ID,
+  TOKEN_PROGRAM_ID,
+} from "@solana/spl-token";
+import {
+  MetadataProgram,
+  DataV2,
+  Metadata,
+  MasterEdition,
+} from "@metaplex-foundation/mpl-token-metadata";
+import { MetadataDemo } from "../target/types/metadata_demo";
 
-describe('metadata-demo', () => {
-
-  const provider = anchor.Provider.env();
+describe("metadata-demo", () => {
+  const provider = anchor.AnchorProvider.env();
   // Configure the client to use the local cluster.
   anchor.setProvider(provider);
 
   const program = anchor.workspace.MetadataDemo as Program<MetadataDemo>;
 
-  it('Initialize', async () => {
-    const tx = await program.rpc.initialize({});
+  it("Initialize", async () => {
+    const tx = await program.methods.initialize().rpc();
     console.log("Your transaction signature", tx);
   });
 
-  it('Create metadata', async () => {
+  it("Create metadata", async () => {
     const mint = anchor.web3.Keypair.generate();
-    const payer = program.provider.wallet.publicKey;
+    const payer = provider.wallet.publicKey;
 
-    const [authority] = (await anchor.web3.PublicKey.findProgramAddress([
-      Buffer.from("auth"),
-    ], program.programId));
+    const [authority] = await anchor.web3.PublicKey.findProgramAddress(
+      [Buffer.from("auth")],
+      program.programId
+    );
 
-    const tokenAccount = (await anchor.web3.PublicKey.findProgramAddress([
-      authority.toBuffer(),
-      TOKEN_PROGRAM_ID.toBuffer(),
-      mint.publicKey.toBuffer()
-    ], ASSOCIATED_TOKEN_PROGRAM_ID))[0];
+    const tokenAccount = (
+      await anchor.web3.PublicKey.findProgramAddress(
+        [
+          authority.toBuffer(),
+          TOKEN_PROGRAM_ID.toBuffer(),
+          mint.publicKey.toBuffer(),
+        ],
+        ASSOCIATED_TOKEN_PROGRAM_ID
+      )
+    )[0];
 
     const data = new DataV2({
       name: "Collection",
@@ -38,21 +51,25 @@ describe('metadata-demo', () => {
       sellerFeeBasisPoints: 1000,
       creators: null,
       collection: null,
-      uses: null
+      uses: null,
     });
 
     const metadataAccount = await Metadata.getPDA(mint.publicKey);
     const editionAccount = await MasterEdition.getPDA(mint.publicKey);
 
-    // @ts-ignore
-    const tx = await program.methods.createMasterEdition(data, true, null).accounts({
-      authority,
-      mint: mint.publicKey,
-      tokenAccount,
-      metadataAccount,
-      editionAccount,
-      metadataProgram: MetadataProgram.PUBKEY,
-    }).signers([mint]).rpc();
+    const tx = await program.methods
+      // @ts-ignore
+      .createMasterEdition(data, true, null)
+      .accounts({
+        authority,
+        mint: mint.publicKey,
+        tokenAccount,
+        metadataAccount,
+        editionAccount,
+        metadataProgram: MetadataProgram.PUBKEY,
+      })
+      .signers([mint])
+      .rpc();
     console.log("Your transaction signature", tx);
-  })
+  });
 });
